36bdf2bd24854cc21df8cd17c13744916aeecd9f
i-bilge <iismailbilge@gmail.com>
2022-06-10 20:20:10 +0200

    bug of adviceLine is solved.

--- app.js
+++ app.js
@@ -14,19 +14,19 @@ btn.addEventListener("click", () => {
     alert("Please give a number between 0 - 100(exclusive)");
   } else if (inputNum == trueNum) {
     output.textContent = `Congragulation ${inputNum} is correct!`;
-    adviceLine.textContent = `SUPER!!!`;
+    adviceLine.textContent = `SUPER!!! in ${count}. try`;
   } else {
     output.textContent = `Sorry ${inputNum} is not correct! Try it again...`;
     count = count + 1;
     attempts.innerHTML = count;
 
     if (inputNum > trueNum) {
-        adviceLine.textContent = `!Enter a number between ${advicePoint1} and ${inputNum}`;
-        if (inputNum < advicePoint2) advicePoint2 = inputNum;
+      if (inputNum < advicePoint2) advicePoint2 = inputNum;
+      adviceLine.textContent = `!Enter a number between ${advicePoint1} and ${advicePoint2}`;
     } 
     else
-        adviceLine.textContent = `!Enter a number between ${inputNum} and ${advicePoint2}`;
-        if (inputNum > advicePoint1) advicePoint1 = inputNum;
+      if (inputNum > advicePoint1) advicePoint1 = inputNum;
+      adviceLine.textContent = `!Enter a number between ${advicePoint1} and ${advicePoint2}`;
   }
   console.log(trueNum);
 });
